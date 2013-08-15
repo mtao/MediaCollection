b130e118d3978ae6b429a9be5ccd55e3ef3a3271
Michael Tao <mtao@cs.toronto.edu>
2013-08-14 22:12:05 -0400

    scanner does database stuff not sure what else i edited months ago

--- include/scanner.h
+++ include/scanner.h
@@ -1,23 +1,37 @@
 #ifndef SCANNER_H
 #define SCANNER_H
 #include <QFileSystemWatcher>
+#include <QSqlDatabase>
+#include <QSqlError>
+#include <QFile>
 #include <QWidget>
+#include <QDir>
 
 class Scanner: public QObject{
     Q_OBJECT
     public:
         Scanner(const QString & root);
         Scanner(const QStringList & rootpaths);
+        ~Scanner();
     private:
-        void recursiveAddPaths(const QString & path, unsigned int depth=0);
-        QFileSystemWatcher * m_watcher;
+        void initializeWatcher();
+        void initializeDatabase();
+
+
+
+        void checkDir(int rootid, const QString & path, unsigned int depth=0);
+        QFileSystemWatcher m_watcher;
+        QSqlDatabase m_db;
+        QDir m_dir;
         unsigned int m_max_depth=5;
         int findDepth(const QString & path);
-        void manualDirCheck(const QString & path);
 
 public slots:
         void fileChanged(const QString &);
-        void dirChanged(const QString &);
+        void rootChanged(const QString &);
+        void printRoots() const;
+        void printFollowed() const;
+        void addRootDir(const QString &);
 signals:
         void newFile(const QString &);
 };
--- src/scanner.cpp
+++ src/scanner.cpp
@@ -1,54 +1,203 @@
-#include "../include/scanner.h"
+#include "include/scanner.h"
 #include <QStringList>
 #include <QtCore/QFileInfo>
 #include <QDir>
 #include "drivers/music/include/tag.h"
+#include <QDateTime>
 #include <QDebug>
+#include <QSqlQuery>
 
-
+QString concatenateToPath(std::initializer_list<QString> list) {
+    QString result(*list.begin());
+    for(const QString * it = list.begin()+1; it != list.end(); ++it) {
+        result = result.append(QDir::separator()).append(*it);
+    }
+    return result;
+}
+static const QString configDir(
+        concatenateToPath(
+            {QDir::home().path(),".config","MediaCollection"}
+            )
+        );
 
 Scanner::Scanner(const QString & root): Scanner(QStringList{root}) {}
-Scanner::Scanner(const QStringList & rootlist)
-    : m_watcher(new QFileSystemWatcher(this)) {
-        connect(m_watcher, SIGNAL(fileChanged(const QString &)), this, SLOT(fileChanged(const QString &)));
-        connect(m_watcher, SIGNAL(directoryChanged(const QString &)), this, SLOT(dirChanged(const QString &)));
 
-        for(auto&& path: rootlist) {
-            recursiveAddPaths(path);
-        }
-    for(auto&& a: m_watcher->directories()) {
-        qWarning() << a;
+    Scanner::Scanner(const QStringList & rootlist)
+: m_watcher()
+    , m_db(QSqlDatabase::addDatabase("QSQLITE"))
+    , m_dir(QDir::currentPath())
+{
+    connect(&m_watcher, SIGNAL(fileChanged(const QString &)), this, SLOT(fileChanged(const QString &)));
+    connect(&m_watcher, SIGNAL(directoryChanged(const QString &)), this, SLOT(rootChanged(const QString &)));
+    connect(&m_watcher, SIGNAL(pathCreated(const QString &)), this, SLOT(fileChanged(const QString &)));
+    connect(&m_watcher, SIGNAL(pathDeleted(const QString &)), this, SLOT(fileChanged(const QString &)));
+
+
+    if(!m_dir.mkpath(configDir)) {
+        qWarning() << "Could not create config dir!!!";
     }
 
-    for(auto&& a: m_watcher->files()) {
-        qWarning() << a;
+    initializeDatabase();
+    //First initialize old watches, then new ones
+    initializeWatcher();
+    for(auto&& root: rootlist) {
+        addRootDir(root);
     }
+
+}
+
+
+
+void Scanner::addRootDir(const QString & path) { 
+    QSqlQuery query;
+    bool ret=query.exec(QString("insert into scanRoots values(NULL,'%1') ").arg(path));
+    if(!ret) {
+        qWarning() << "Failed: " << query.lastQuery();
+        qWarning() << "Failed: " << query.lastError();
     }
-void Scanner::recursiveAddPaths(const QString & path, unsigned int depth) {
-    if(depth > m_max_depth) return;
-    QFileInfo fileInfo(path);
-    if (fileInfo.isDir()) {
-    m_watcher->addPath(path);
-        QDir dir(path);
-        QStringList fileList = dir.entryList(
-                QDir::AllEntries | QDir::NoDotAndDotDot
-                | QDir::NoSymLinks | QDir::Hidden, QDir::Name
-                | QDir::IgnoreCase);
-        for (int i = 0; i < fileList.count(); ++i) {
-            recursiveAddPaths(path + tr("/")+fileList.at(i),depth+1);
-        }
-    } else {
-        /*
-        Tag t(path);
+}
+
+void Scanner::printRoots() const {
+    QSqlQuery query(QString("select * from scanRoots"));
+    while(query.next()) {
         qWarning() 
-            << t.title() << " " 
-            << t.artist() << " " 
-            << t.album() << " " 
-            << t.genre() << " " 
-            << t.comment() << " " 
-            << t.track();
-        */
+            << query.value(0).toInt()
+            << " " << query.value(1).toString()
+            ;
     }
+}
+
+void Scanner::printFollowed() const {
+    QSqlQuery query(QString("select * from scanFileCache"));
+    while(query.next()) {
+        qWarning() 
+            << query.value(0).toInt()
+            << " " << query.value(1).toInt()
+            << " " << query.value(2).toString()
+            << " " << query.value(2).toDateTime().toString(Qt::ISODate);
+            ;
+    }
+}
+
+
+void Scanner::initializeWatcher() {
+    QSqlQuery query(QString("select * from scanRoots"));
+    QStringList rootlist;
+    while(query.next()) {
+        int id = query.value(0).toInt();
+        QString path = query.value(1).toString();
+
+        m_watcher.addPath(path);
+
+        QDir::setCurrent(path);
+        checkDir(id,".");
+        QDir::setCurrent(m_dir.absolutePath());
+    }
+    /*
+    for(auto&& a: m_watcher.directories()) {
+        qWarning() << a;
+    }
+
+    for(auto&& a: m_watcher.files()) {
+        qWarning() << a;
+    }
+    */
+}
+
+
+
+void Scanner::initializeDatabase() {
+    QString dbpath(QDir::toNativeSeparators(
+                concatenateToPath({configDir,"scanner.db"})
+                ));
+
+    m_db.setDatabaseName(dbpath);
+    if(!m_db.open()) 
+    {
+        qWarning() << "Could not open database!";
+        return;//Dont expect anything after this to work...
+    }
+
+    QSqlQuery query;
+    bool ret = query.exec(
+            "create table if not exists scanRoots "
+            "(id integer primary key,"
+            "path varchar(128) not null unique"
+            ")"
+            );
+    if(ret) {
+        qWarning() << "Was able to create scanroots table or it already existed";
+    } else {
+        qWarning() << "Failed to create scanroots table";
+    }
+    bool ret2 = query.exec(
+            "create table if not exists scanFileCache "
+            "(id integer primary key,"
+            "rootid integer,"// foreign key references scanRoots(id),"
+            "path varchar(128) not null,"
+            "lastmodified timestamp,"//To keep track of modifications since last launch
+            "foreign key(rootid) references scanRoots(id)"// foreign key references scanRoots(id),"
+            ")"
+            );
+    if(ret2) {
+        qWarning() << "Was able to create scandirs table or it already existed";
+    } else {
+        qWarning() << "Failed to create scandirs table";
+        qWarning() << query.lastError();
+    }
+
+
+
+}
+
+void Scanner::checkDir(int rootid, const QString & path, unsigned int depth) {
+    if(depth > m_max_depth) return;
+    qWarning () << "Checking the dir " << path ;
+    QFileInfo fileInfo(path);
+    QDateTime lastMod = fileInfo.lastModified();
+    //TODO: we're doing string comparison here, which is bad :(
+    QSqlQuery query(QString("select id,lastmodified from scanFileCache where rootid = %1 and path = '%2'")
+            .arg(rootid).arg(path));
+    bool dirty = false;
+    int row;//bad data unless dirty is true
+    if(!query.next()) {//not in db yet so add
+        bool ret = query.exec(QString("insert into scanFileCache values(NULL,%1,'%2','%3')")
+                .arg(rootid).arg(path).arg(lastMod.toString(Qt::ISODate)));
+        if(!ret) {
+            qWarning() << "Was unable to execute the following sql query:" << query.lastQuery();
+        }
+        dirty = true;
+    } else {
+        row = query.value(0).toInt();
+        dirty = query.value(1).toDateTime() < lastMod;
+        qWarning() <<"Date comparison: " << dirty << " "<< query.value(1).toString() << " " << lastMod.toString();
+    }
+    if(dirty) {
+        if (fileInfo.isDir()) {
+
+
+            QDir dir(path);
+            QStringList fileList = dir.entryList(
+                    QDir::AllEntries | QDir::NoDotAndDotDot
+                    | QDir::NoSymLinks | QDir::Hidden, QDir::Name
+                    | QDir::IgnoreCase);
+            for (int i = 0; i < fileList.count(); ++i) {
+                checkDir(rootid, path + tr("/")+fileList.at(i),depth+1);
+            }
+        } else {
+            //TODO: Update metadata...
+        }
+        //Update row for timestamp after we've updated this...
+        bool ret = query.exec(QString("update scanFileCache set lastmodified='%1' where id=%2")
+                .arg(lastMod.toString(Qt::ISODate)).arg(row));
+        if(!ret) {
+            qWarning() << "Was unable to execute the following sql query:" << query.lastQuery();
+        }
+    }
+}
+
+Scanner::~Scanner() {
+    m_db.close();
 }
 
 
@@ -56,45 +205,26 @@ void Scanner::recursiveAddPaths(const QString & path, unsigned int depth) {
 //This should never be called because we are assuming that scanning all files will waste all inotify :(
 void Scanner::fileChanged(const QString & filepath) {
     qWarning()<<"File: " << filepath;
+    static_assert(true, "Shouldn't scan for files themselves");
 }
 
 //Note:
 //Scanner will detect files being added but collection has to notice files not existing
 
 //TODO: deal with stuff being deleted
-void Scanner::dirChanged(const QString & path) {
-    QDir dir(path);
-    QStringList fileList = dir.entryList(
-            QDir::AllEntries | QDir::NoDotAndDotDot
-            | QDir::NoSymLinks | QDir::Hidden, QDir::Name
-            | QDir::IgnoreCase);
-    //Add all directories not in the list of paths
-    for(const QString & name: fileList) {
-        QFileInfo info(name);
-        if(info.isDir()) {
-            if(m_watcher->directories().indexOf(name)==-1) {//TODO: Scans whole list of directories, should make faster
-                recursiveAddPaths(path, findDepth(path));
-            }
-
-        } else {
-            emit newFile(name);
-        }
+void Scanner::rootChanged(const QString & path) {
+    QSqlQuery query(QString("select id from scanRoots where path='%1'").arg(path));
+    if(query.next()) {
+        QDir::setCurrent(path);
+        checkDir(query.value(0).toInt(),".");
+        QDir::setCurrent(m_dir.absolutePath());
     }
-    /*
-    for (int i = 0; i < fileList.count(); ++i) {
-        QString&& str = fileList
-        recursiveAddPaths(path + tr("/")+fileList.at(i),depth+1);
-    }
-    */
-}
-
-void Scanner::manualDirCheck(const QString & filepath) {
 }
 
 
 int Scanner::findDepth(const QString & path) {
     int min=m_max_depth + 1;
-    for(QString & dirname: m_watcher->directories()) {
+    for(QString & dirname: m_watcher.directories()) {
         QDir dir(dirname);
         int depth = dir.relativeFilePath(path).split(tr("/")).size();
         if(depth < min)
